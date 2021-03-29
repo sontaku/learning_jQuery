aca63ce584fd00f539f2c63413cf92ba7ac72752
jyson19 <jyson19@gmail.com>
2021-03-29 16:25:12 +0900

    Update jQuery.js

--- WebUI/WebContent/5_jquery_class/1_css_event/jquery.js
+++ WebUI/WebContent/5_jquery_class/1_css_event/jquery.js
@@ -6,7 +6,8 @@ $(function(){
 	$('#hideButton').click(function() {
 		console.log("숨기기")
 		//$('#pic').hide();
-		$('#pic').animate({'width':0, 'height':0}, 1000, 'linear');
+		//$('#pic').animate({'width':0, 'height':0, 'opacity' : 0}, 1000, 'linear');
+		$('#pic').animate({'opacity' : 0}, 1000, 'linear');
 		
 	})
 	
@@ -14,7 +15,8 @@ $(function(){
 	$('#showButton').click(function() {
 		console.log("보이기")
 		//$('#pic').css('display', '');
-		$('#pic').animate({'width':500, 'height':269}, 1000, 'linear');
+		//$('#pic').animate({'width':500, 'height':269, 'opacity' : 1}, 1000, 'linear');
+		$('#pic').animate({'opacity' : 1}, 1000, 'linear');
 	})
 	
 	//---------------------------------------------------------
@@ -22,6 +24,9 @@ $(function(){
 	//		한 번 클릭하면 감추기 다시 클릭하면 보이게
 	$('#toggleButton').click(function() {
 		console.log("감추기보이기")
-		$('#pic').toggle();
+		//$('#pic').toggle();
+		//if($('#pic').css('opacity') == 1) $('#pic').animate({'opacity' : 0}, 1000, 'linear');
+		//if($('#pic').css('opacity') == 0) $('#pic').animate({'opacity' : 1}, 1000, 'linear');
+		$('#pic').css('opacity') == 1 ? $('#pic').animate({'opacity' : 0}, 1000, 'linear') : $('#pic').animate({'opacity' : 1}, 1000, 'linear')
 	})
 })
